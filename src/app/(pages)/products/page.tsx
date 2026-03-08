ef1ef614825ada209e479e68d4bdad8205ed064c
Mohamed Ahmed <mvchnccc@gmail.com>
2026-03-08 20:47:08 +0200

    update

--- src/app/(pages)/products/page.tsx
+++ src/app/(pages)/products/page.tsx
@@ -33,7 +33,7 @@ export default async function AllProducts() {
 
   return (
     <div className="container mx-auto p-4">
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-4">
         {data.data.map((product) => (
           <ProductCard key={product._id} product={product} favIds={favIds} CartIds={cartIds} />
         ))}
